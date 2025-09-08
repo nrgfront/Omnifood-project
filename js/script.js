359dc26d42bafd1dd8e257099d4858ee09fe8a0d
nrgfront <narges.fakhri1372@gmail.com>
2025-09-08 10:25:19 +0330

    Update script.js
    feat: add scroll-to-top button with smooth animation

--- js/script.js
+++ js/script.js
@@ -91,4 +91,21 @@ AOS.init({
   once: true, 
 });
 
+///////////////////////////////////////////////////////////
+// Button to top
+
+const btnTop = document.getElementById("btn-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 500) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+});
+
+btnTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
 
